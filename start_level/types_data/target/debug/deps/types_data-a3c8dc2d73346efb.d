6670d188d61b44f98ab79b633bb029ffc0b37fe4
BrunoMarques <blmarques.dev@gmail.com>
2023-07-23 20:32:10 -0300

    (cargo): learning to create project with cargo new 'project' and cargo init

---
+++ start_level/types_data/target/debug/deps/types_data-a3c8dc2d73346efb.d
@@ -0,0 +1,5 @@
+/Volumes/Projects/WORKSPACES/PERSONAL/RUST/learn/start_level/types_data/target/debug/deps/types_data-a3c8dc2d73346efb: src/main.rs
+
+/Volumes/Projects/WORKSPACES/PERSONAL/RUST/learn/start_level/types_data/target/debug/deps/types_data-a3c8dc2d73346efb.d: src/main.rs
+
+src/main.rs:
---
+++ start_level/types_data/target/debug/types_data.d
@@ -0,0 +1,1 @@
+/Volumes/Projects/WORKSPACES/PERSONAL/RUST/learn/start_level/types_data/target/debug/types_data: /Volumes/Projects/WORKSPACES/PERSONAL/RUST/learn/start_level/types_data/src/main.rs
