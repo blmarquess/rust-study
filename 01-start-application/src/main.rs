5a98fa930cb07896c16b633f55080925d70098c8
BrunoMarques <blmarques.dev@gmail.com>
2023-07-23 20:25:13 -0300

    (variables): how to set variable mutable

--- 01-start-application/src/main.rs
+++ 01-start-application/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let name = "Rust";
+    println!("Hello, {}!", name);
+    println!(
+        "por padrão as variaveis são imutaveis (constantes)\n
+        para tornar uma variavel mutavel é necessario usar o modificador mut "
+    );
+    let mut mutableVariable = "Rust";
+    println!("Hello, {}!", mutableVariable);
+    mutableVariable = "Rust 2";
+    println!("Hello, {}!", mutableVariable);
 }
