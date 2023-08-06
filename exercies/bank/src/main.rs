2c9cfa4f5a17974c255d3dc66ac683fa873766cc
Bruno Marques <blmarques.dev@gmail.com>
2023-08-05 21:39:42 -0300

    feat(io): create menu interaction to learn of get inputs and convert types
    this is a exersice of IO and convesion types and use matchers

--- exercies/bank/src/main.rs
+++ exercies/bank/src/main.rs
@@ -18,4 +18,47 @@ fn main() {
 
     println!("Esta é a conta do! {:?}", acc_joe);
     println!("Esta é a conta da! {:?}", acc_jane);
+
+    let mut you_name: String = String::new();
+    let mut you_password: String = String::new();
+
+    println!("Digite seu nome: ");
+    std::io::stdin()
+        .read_line(&mut you_name)
+        .expect("Falha ao ler o nome");
+    println!("Digite seu deposito inicial: ");
+    let mut input_money: String = String::new();
+    std::io::stdin()
+        .read_line(&mut input_money)
+        .expect("Falha ao ler o saldo");
+    let you_balance: f64 = input_money.trim().parse().expect("Falha ao ler o saldo");
+
+    println!("Digite seu tipo de conta: ");
+    let mut input_acc_type: String = String::new();
+    std::io::stdin()
+        .read_line(&mut input_acc_type)
+        .expect("Falha ao ler o tipo de conta");
+
+    let account_type: AccountType = match input_acc_type.trim().parse() {
+        Ok(num) => match num {
+            1 => AccountType::Checking,
+            2 => AccountType::Savings,
+            _ => panic!("Tipo de conta inválido"),
+        },
+        Err(_) => panic!("Tipo de conta inválido"),
+    };
+
+    println!("Digite sua senha: ");
+    std::io::stdin()
+        .read_line(&mut you_password)
+        .expect("Falha ao ler a senha");
+
+    let acc_you = Account::new(
+        you_name.to_string(),
+        you_balance,
+        account_type,
+        you_password.to_string(),
+    );
+
+    println!("Esta é a conta do {} {:?}", acc_you.name, acc_you);
 }
